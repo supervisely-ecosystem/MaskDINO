dfa4783254546f570d88ff41b8a50b68bcc9fadb
MaxTeselkin <tesmaxxxx@gmail.com>
2025-07-30 09:37:14 +0000

    added quiet multiscale attn installation

--- serve/src/main.py
+++ serve/src/main.py
@@ -2,7 +2,9 @@ import subprocess
 import sys
 
 subprocess.run(["sh", "install_multiscale_attn.sh"])
-subprocess.run([sys.executable, "-m", "pip", "install", "Pillow==10.1.0"], check=True)
+subprocess.run(
+    [sys.executable, "-m", "pip", "install", "--quiet", "Pillow==10.1.0"], check=True
+)
 from dotenv import load_dotenv
 import supervisely as sly
 from serve.src.maskdino_model import MaskDinoModel
