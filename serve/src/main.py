b972d58089e5e4de871da43b8d58137f1090add0
MaxTeselkin <tesmaxxxx@gmail.com>
2025-07-30 06:22:11 +0000

    uncommented necessary line

--- serve/src/main.py
+++ serve/src/main.py
@@ -1,7 +1,7 @@
 import subprocess
 import sys
 
-# subprocess.run(["sh", "install_multiscale_attn.sh"])
+subprocess.run(["sh", "install_multiscale_attn.sh"])
 subprocess.run([sys.executable, "-m", "pip", "install", "Pillow==10.1.0"], check=True)
 from dotenv import load_dotenv
 import supervisely as sly
